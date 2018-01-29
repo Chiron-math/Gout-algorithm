a3b4dab7ffa9c034dc053ef9901cd20917f885b3
Vioswang <vitoswang94@Outlook.com>
2018-01-30 02:59:31 +0800

    ErebusKing
    程序员算法趣题Q：01回文十进制数

--- math-practise/isPalindrom.java
+++ math-practise/isPalindrom.java
@@ -1,9 +1,12 @@
 package com.company;
 
 /**
- * 程序员算法趣题Q：01
+ * 程序员算法趣题Q：01回文十进制数
+ * 求用十进制、二进制、八进制表示都是回文数的所有数字中，大于十进制数10的最小值
  * Created by admin on 2018/1/25.
  * 修改一步在deciToX()里，时间2018/1/25
+ * 修改deciToX()，完善逆序功能和数值的范围过小导致的溢出问题
+ * 将输入的类型都转为String
  */
 public class isPalindrom {
 
@@ -15,11 +18,11 @@ public class isPalindrom {
 	 * @param i
 	 * @return
 	 */
-	public Boolean isPalin(long i){
+	public Boolean isPalin(String i){
 		boolean b = true;
 		int m = 0;
-		char[] d = String.valueOf(i).toCharArray();
-		int digit = String.valueOf(i).length()-1;
+		char[] d = i.toCharArray();
+		int digit = i.length()-1;
 		while(m < digit && d[m] == d[digit-m])
 			m++;
 		//之前在m判断里m < (digit+1)/2-1在偶数是有问题
@@ -30,48 +33,31 @@ public class isPalindrom {
 
 
 	/**
-	 * do {
-			count=10*(count+i%scale);
-			i=i/scale;
-		} while (i>scale);
-		count +=i/scale;
-		count =count/10;
-		结果会倒过来而且有问题
-	 */
-	/**
-	 * 十进制转化为X进制
-	 * 将余数放后，最后得到的除数放在作为位数标记m的位置上（即是首位）
-	 * 上个版本默认当下的进制是10，只能10->X进制
-	 * 这样不会出现倒过来的情况
-	 * Mtah.pow指数运算(10,m)10的m次运算
+	 * 默认不逆序，只是单纯的M进制->N进制
 	 * @param i
 	 * @return
 	 */
-
-	public long deciToX(long num,int scale_start, int scale_end){
-		long revdat=0, count=0;
-		int i=-1;
-		//更简单方法，直接用String的连接特性，省去相乘的问题
+	public static String deciToX(String a,int scale_start, int scale_end){
+		char[] b = a.toCharArray();
+		long revdat=0;
+		int i=0;
 		String cc = "";
 
+		//将给的字符串数值按照进制转换为10进制
 		do {
+			revdat = (long)Math.pow(scale_start,a.length()-i-1)*((b[i]-'0')%scale_start)+revdat;
+//			System.out.println(revdat+"第"+i+"次");
 			i++;
-			revdat = (long)Math.pow(scale_start,i)*(num%scale_start)+revdat;
-			num /= 10;
-		} while (num>0);
-
-		do {
-			//cc +=  revdat%scale_end;会使得数字倒过来了
-			cc =  revdat%scale_end+cc;
-			revdat/=scale_end;
-		} while (revdat>0);
-		count = Long.parseLong(cc.trim());
-		System.out.println(""+count);
-		return count;
+		} while (i<a.length());
+//		System.out.println("转换后的数值"+revdat);
+			do {
+				cc =  revdat%scale_end+cc;
+				revdat/=scale_end;
+			} while (revdat>0);
+		return cc;
 	}
-
 	/**
-	 * 考虑逆序的功能要求和如果数值过大出现的数值类型内存溢出的情况，那转为String输出型
+	 * 考虑逆序的功能要求和如果数值过大，那转为String输出型
 	 * 这样就不会因为大的进制转换成小的导致值溢出
 	 * @param a
 	 * @param scale_start
@@ -85,15 +71,13 @@ public class isPalindrom {
 		int i=0;
 		String cc = "";
 
-		//将给的字符串数值按照进制转换为10进制
 		do {
 			revdat = (long)Math.pow(scale_start,a.length()-i-1)*((b[i]-'0')%scale_start)+revdat;
-			System.out.println(revdat+"第"+i+"次");
+//			System.out.println(revdat+"第"+i+"次");
 			i++;
 		} while (i<a.length());
-		System.out.println("转换后的数值"+revdat);
+//		System.out.println("转换后的数值"+revdat);
 
-		//根据是否需要逆序的要求，再转换为X进制
 		if (nixu) {
 			do {
 				cc +=  revdat%scale_end;
@@ -105,7 +89,7 @@ public class isPalindrom {
 				revdat/=scale_end;
 			} while (revdat>0);
 		}
-		System.out.println(cc);
+//		System.out.println(cc);
 		return cc;
 	}
 
@@ -117,16 +101,16 @@ public class isPalindrom {
 
     	isPalindrom ip = new isPalindrom();
     	int cc =0;
-    	long m ,n ;
-    		System.out.println(ip.isPalin(121));
+    	String m ,n ;
+    		System.out.println(ip.isPalin("121"));
     		do{
     			cc++;
     			i +=2;
     			//因为有二进制，最高位必定是1所以求回文的结果也一定是1
     			//得到结论，所求的数值是奇数
-    			m = ip.deciToX(i, 10,2);
-    			n = ip.deciToX(i, 10,8);
-    		}while(!ip.isPalin(m)||!ip.isPalin(n)||!ip.isPalin(i));
+    			m = ip.deciToX(i+"", 10,2);
+    			n = ip.deciToX(i+"", 10,8);
+    		}while(!ip.isPalin(m)||!ip.isPalin(n)||!ip.isPalin(i+""));
     		System.out.println(m+" "+ip.isPalin(m));
     		System.out.println(n+" "+ip.isPalin(n));
     		System.out.println("结果是："+i);
